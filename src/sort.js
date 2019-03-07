156045cab4e7a969a7bb66d190498bb121ee5f67
Nikoloz Razmadze <raloz1@gmail.com>
2019-03-08 01:52:35 +0400

    Rewrite sorting logic

--- src/sort.js
+++ src/sort.js
@@ -1,121 +1,85 @@
-let tags        = {};
-let weighs      = {};
-let sorted      = {};
-let couples     = {};
-let sortedArray = [];
-let links       = [];
+const _ = require('lodash');
 
-function sort(data) {
-  index(data);
-  weigh();
-  srt();
-  sortToArray();
-  couple();
-  link(sortedArray[0].id, links, 0);
+function sort(slides, _maximize = false) {
 
-  const sorted = [];
+  const push = (i) => {
+    const s2 = slides.splice(i, 1)[0];
+    sorted.push(s2);
+    return s2;
+  };
 
-  links.forEach((i, j) => {
-    sorted[j] = data[i];
-  });
+  let target = 0;
+
+  if (!_maximize) {
+    target = average(slides, 1000);
+  }
+
+  let s1 = slides.pop();
+  const sorted = [ s1 ];
+
+  outer: while (slides.length) {
+
+    let max = 0;
+
+    for (let i = slides.length - 1; i >= 0; i--) {
+
+      const s2 = slides[i];
+      const weight = weigh(s1, s2);
+
+      if (!_maximize) {
+        if (weight >= target) {
+          s1 = push(i);
+          continue outer;
+        }
+      }
+
+      max = max < weight ? i : max;
+    }
+
+    s1 = push(max);
+
+  }
 
   return sorted;
+
 }
 
-function index(data){
-
-
-
-  for(let i in data){
-
-    for(let j in data[i].tags){
-      if(!tags.hasOwnProperty(data[i].tags[j])){
-        tags[data[i].tags[j]] = [];
-      }
-
-      tags[data[i].tags[j]].push(i);
-    }
-  }
+function weigh({ tags: t1 }, { tags: t2 }) {
+  const intersection = _.intersection(t1, t2);
+  const d1 = _.difference(t1, intersection);
+  const d2 = _.difference(t2, intersection);
+  return Math.min(intersection.length, d1.length, d2.length);
 }
 
-function weigh(){
-  for(let i in tags){
+function average(slides, sampleSize = 100) {
 
-    for(let j = 0; j < tags[i].length; j++){
-      let left = tags[i][j];
-      for(let k = j + 1; k < tags[i].length; k++){
+  const indices = {};
+  const length = Math.min(slides.length, sampleSize);
 
-        let right = tags[i][k];
-        let key   = left + "-" + right;
-        if(!weighs.hasOwnProperty(key)){
-            weighs[key] = 0;
-        }
-        weighs[key]++;
-      }
-    }
-  }
-}
+  for (let i = 0; i < length; i++) {
 
-function srt(){
-  for(let i in weighs){
-    let keys = i.split('-');
-    if(!sorted.hasOwnProperty(keys[0])){
-      sorted[keys[0]] = 0;
-    }
-    if(!sorted.hasOwnProperty(keys[1])){
-      sorted[keys[1]] = 0;
-    }
-    sorted[keys[0]]++;
-    sorted[keys[1]]++;
-  }
-}
+    let key;
 
-function couple(){
-  for(let i in weighs){
-    let keys = i.split('-');
-    if(!couples.hasOwnProperty(keys[0])){
-      couples[keys[0]] = [];
-    }
-    if(!couples.hasOwnProperty(keys[1])){
-      couples[keys[1]] = [];
-    }
-    couples[keys[0]].push(keys[1]);
-    couples[keys[1]].push(keys[0]);
-  }
-}
+    do {
+      key = Math.floor(Math.random() * slides.length);
+    } while (indices[key]);
 
-function sortToArray(){
-  for(let i in sorted){
-    sortedArray.push({id: i, connections: sorted[i]});
+    indices[key] = slides[key];
   }
 
-  sortedArray.sort(function(a, b){
-    return a.connections - b.connections;
-  });
-}
+  const picked = Object.keys(indices).map(id => indices[id]);
 
-function link(id, l, s){
+  const weights = [];
 
-  if(couples[id] === undefined ){
-    return;
-  }
-  let connections = couples[id];
-  let neighbor    = connections[0];
-
-  for(let j of connections){
-    let myIndex = couples[j].indexOf(id);
-
-    if(myIndex > -1){
-      couples[j].splice(myIndex, 1);
-    }
-    if(sorted[j] < neighbor){
-      neighbor = j;
+  for (let i = 0; i < picked.length; i++) {
+    const p1 = picked[i];
+    for (let j = i + 1; j < picked.length; j++) {
+      const p2 = picked[j];
+      weights.push(weigh(p1, p2));
     }
   }
 
-  l.push(id);
-
-  link(neighbor, l, s+1)
+  return Math.floor(weights.reduce((a, v) => a + v, 0) / weights.length);
 
 }
 
